b3aa7d8d8ffd720a35b2dd4b58dccd23a0665711
Kheerthana <kheerthana.20@gmail.com>
2024-09-04 14:37:49 +0530

    this is my second commit

---
+++ C2TC_kheerthyyy/src/com/tns/daytwo/conditionalaprogram.java
@@ -0,0 +1,31 @@
+package com.tns.daytwo;
+
+public class conditionalaprogram {
+public static void main(String[] args) {
+
+		
+
+		int number = 5; 
+
+
+
+        if (number > 0) {
+
+            System.out.println("The number is positive.");
+
+        } else if (number < 0) {
+
+            System.out.println("The number is negative.");
+
+        } else {
+
+            System.out.println("The number is zero.");
+
+        }
+
+
+
+	}
+}
+
+
---
+++ C2TC_kheerthyyy/src/com/tns/daytwo/loopingprogram.java
@@ -0,0 +1,27 @@
+package com.tns.daytwo;
+
+public class loopingprogram {
+	public static void main(String[]args) {
+		// For loop
+        System.out.println("For loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // While loop
+        System.out.println("\nWhile loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+
+        
+
+
+
+}
+
+}
+
+
