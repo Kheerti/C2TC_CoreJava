fda255f73f9016ee026a12fad6575d40d907e3ee
Kheerthana <kheerthana.20@gmail.com>
2024-09-04 12:07:52 +0530

    this is my first commit

---
+++ C2TC_kheerthyyy/src/com/tns/dayone/FirstProgram.java
@@ -0,0 +1,17 @@
+package com.tns.dayone;
+
+public class FirstProgram {
+	
+	public static void main(String[] args){
+	int a=10;  
+	int b=5; 
+	
+	
+    System.out.println(a+b);  
+	System.out.println(a-b);  
+	System.out.println(a*b);  
+	System.out.println(a/b);  
+	System.out.println(a%b);
+
+	}
+}
